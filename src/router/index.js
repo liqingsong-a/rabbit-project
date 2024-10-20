3bc19f97402e094f10f939ff9c35ce53189db82f
liqingsong-a <16627629245@163.com>
2024-10-20 20:28:20 +0800

    完成了router一级二级配置

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,36 @@
+// createRouter：创建router实例对象
+// createWebHistory:创建history模式的路由对象
+// routes:路由规则数组
+
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Login from '@/views/Login/index.vue'
+import Layout from '@/views/Layout/index.vue'
+import Home from '@/views/Home/index.vue'
+import Category from '@/views/Category/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+
+  // 配置path和component对应关系的位置
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
+   {
+    path:'/',
+    component:Layout,
+    children:[ //配置二级路由
+      {
+        path:'',// 空字符串表示默认子路由
+        component:Home
+      },
+      {
+        path:'category',
+        component:Category
+      }
+    ]
+   },
+   {
+    path:'/login',
+    component:Login
+   }
   ]
 })
 
