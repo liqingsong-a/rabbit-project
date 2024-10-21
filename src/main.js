7e38dd9e81001dbc7d394260b7cce7631f3e2450
liqingsong-a <16627629245@163.com>
2024-10-21 15:47:16 +0800

    静态资源的引入和插件的安装

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
-import './assets/main.css'
+//引入初始化样式文件
+import '@/styles/common.scss'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
@@ -11,6 +12,7 @@ import { getCategory } from '@/apis/testAPI'
 getCategory().then(res => {
     console.log(res);
 })
+
 
 const app = createApp(App)
 
