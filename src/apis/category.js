e5a9eb4908103bb89d4c5c8a3fc07a6ab9c5ee7e
liqingsong-a <16627629245@163.com>
2024-10-29 21:02:54 +0800

    二级分类-面包屑导航实现

--- src/apis/category.js
+++ src/apis/category.js
@@ -1,6 +1,7 @@
 // request是和httpInstance一样的，下那个名字都一样的只不过是对axios的二次封装封装，request是对axios的二次封
 import request from '@/utils/http'
 
+
 export function getCategoryAPI(id){
     return request({
         url:'/category',
@@ -10,8 +11,20 @@ export function getCategoryAPI(id){
     })
 }
 
-
-
+// 二级分类的面包屑导航
+/**
+ * @description: 获取二级分类列表数据
+ * @param {*} id 分类id 
+ * @return {*}
+ */
+export const getCategoryFilterAPI = (id) => {
+    return request({
+        url:'/category/sub/filter',
+        params:{
+            id
+        }
+    })
+}
 
 
 
