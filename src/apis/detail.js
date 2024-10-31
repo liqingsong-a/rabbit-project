dc8043c7a5d77964f9467964bf12c1695ed4b437
liqingsong-a <16627629245@163.com>
2024-10-31 18:10:58 +0800

    详情-基础数据渲染

---
+++ src/apis/detail.js
@@ -0,0 +1,19 @@
+import request from '@/utils/http'
+
+export  const getDetail = (id) => {
+    return request({
+        url:'/goods',
+        params:{
+            id
+        }
+    })
+}
+
+
+
+
+
+
+
+
+
