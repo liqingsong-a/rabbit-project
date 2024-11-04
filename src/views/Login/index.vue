9098c12a6f18a8d1cb04773f871fa0249c2ff1a7
liqingsong-a <16627629245@163.com>
2024-11-04 21:21:37 +0800

    登录-pinia管理用户数据

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,11 +1,12 @@
 <script setup>
 import  {ref} from 'vue'
-import {loginAPI} from '@/apis/user'
 import { ElMessage } from 'element-plus'
 import 'element-plus/theme-chalk/el-message.css'
 import { useRouter } from 'vue-router'
+import { useUserStore } from '@/stores/user'
 
 
+const userStore = useUserStore()
 // 表单校验（账户名+密码）
 const form = ref({
     account: '',
@@ -49,8 +50,7 @@ const doLogin = () =>{
         // 以valid作为判断条件 如果通过校验才执行登录逻辑
         if(valid){
             // TODO LOGIN
-          const res =  await loginAPI({ account, password})
-          console.log(res);
+           await userStore.getUserInfo({account,password})
           // 1,提示用户
           ElMessage({type:'success',message:'登录成功'})
           // 2，跳到首页
