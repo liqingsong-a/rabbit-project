df8593802fe3b1aefd06fc2b998e8a036b7fdd8d
liqingsong-a <16627629245@163.com>
2024-11-05 20:51:26 +0800

    401拦截器处理

--- src/utils/http.js
+++ src/utils/http.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 import { ElMessage } from 'element-plus'
 import { useUserStore } from '@/stores/user'
 import 'element-plus/theme-chalk/el-message.css'
-
+import router from '@/router'
 
 const httpInstance = axios.create({
     baseURL: 'http://pcapi-xiaotuxian-front-devtest.itheima.net',
@@ -13,7 +13,7 @@ const httpInstance = axios.create({
 //拦截器
 // axios请求拦截器
 httpInstance.interceptors.request.use(config => {
-    // 1，从pinia获取token数据
+  // 1，从pinia获取token数据
     const userStore = useUserStore()
     // 2按照后端的要求拼接token数据
     const token = userStore.userInfo.token
@@ -25,11 +25,19 @@ httpInstance.interceptors.request.use(config => {
   
   // axios响应式拦截器
   httpInstance.interceptors.response.use(res => res.data, e => {
+    const userStore = useUserStore()
     //同意错误提示
     ElMessage({
       type:'warning',
       message:e.response.data.message
     })
+    // 401token失效处理
+    // 1，清楚本地用户数据
+    // 2,跳转到登录页
+    if(e.response.status === 401){
+      userStore.clearUserInfo()
+      router.push('/login')
+    }
     return Promise.reject(e)
   })
 
